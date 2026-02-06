f044e8026b8cf25f998cd3ab6c7dad6d95568303
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-02-06 20:01:20 +0530

    refactor: Add new Header component to render course name

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,3 +1,14 @@
+const Header = (props) =>{
+  console.log("Header is :" +props.course)
+  return(
+    <>
+    <h1>{props.course}</h1>
+    </>
+  )
+}
+
+
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,7 +20,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
+      <Header course = {course}/>
       <p>
         {part1} {exercises1}
       </p>
