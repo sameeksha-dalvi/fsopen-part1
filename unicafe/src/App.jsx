d72a6b8a47a22c89c47a60b7165c35e84618628a
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-02-12 19:05:30 +0530

    feat: add total, average, and positive feedback statistics (exercise 1.7)

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -5,17 +5,21 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good + neutral + bad;
 
   return (
     <div>
       <h1>Give feedback: </h1>
-      <button onClick={() => setGood(good +1)}>good</button>
+      <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)} >neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
       <h1>Statistics:</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {(good-bad)/total}</p>
+      <p>positive {(good / total) * 100}%</p>
     </div>
   )
 }
