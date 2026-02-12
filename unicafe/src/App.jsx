23e8ddb51c2e4c6cb34801a9290f2cdf75fa7f9c
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-02-12 19:26:31 +0530

    feat: add 'No feedback given' message for empty state (exercise 1.9)

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 const Statistics = (props) => {
   console.log("Statistics props", props);
+
   if (props.text == "positive") {
     return (
       <>
@@ -25,22 +26,38 @@ const App = () => {
   const [bad, setBad] = useState(0)
   const total = good + neutral + bad;
 
-  return (
-    <div>
-      <h1>Give feedback: </h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)} >neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h1>Statistics:</h1>
-      <Statistics text="good" value={good} />
-      <Statistics text="neutral" value={neutral} />
-      <Statistics text="bad" value={bad} />
-      <Statistics text="all" value={total} />
-      <Statistics text="average" value={(good - bad) / total} />
-      <Statistics text="positive" value={(good / total) * 100} />
+  if (total == 0) {
+    return (
+      <div>
+        <h1>Give feedback: </h1>
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)} >neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
+        <h1>Statistics:</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>Give feedback: </h1>
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)} >neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
+        <h1>Statistics:</h1>
+        <Statistics text="good" value={good} />
+        <Statistics text="neutral" value={neutral} />
+        <Statistics text="bad" value={bad} />
+        <Statistics text="all" value={total} />
+        <Statistics text="average" value={(good - bad) / total} />
+        <Statistics text="positive" value={(good / total) * 100} />
 
-    </div>
-  )
+      </div>
+    )
+  }
+
+
+
 }
 
 export default App
